9897fefb97cf11c35a35cdb36c501f12ad8360e1
DungDev <nquocdung2302@gmail.com>
2026-02-25 23:20:53 +0700

    Chore: Ngừng theo dõi thư mục src/models

--- src/models/inputTypeSchemas/UsersScalarFieldEnumSchema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-
-export const UsersScalarFieldEnumSchema = z.enum(['id','name','email','bio','address','phone','avatar','verify','status','createdAt','updatedAt','deletedAt']);
-
-export default UsersScalarFieldEnumSchema;
